c8787de3228015efc04c700e80ad913f146db762
RoederFer <RoederFer@users.noreply.github.com>
2022-05-30 15:27:55 +0200

    Projektdateien hinzufügen.

---
+++ SmartER_Garden/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SmartER_Garden
+{
+    public class Class1
+    {
+    }
+}
---
+++ SmartER_Garden_API/Controllers/SmartER_Garden_Controller.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartER_Garden_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SmartER_Garden_Controller : ControllerBase
+    {
+    }
+}
